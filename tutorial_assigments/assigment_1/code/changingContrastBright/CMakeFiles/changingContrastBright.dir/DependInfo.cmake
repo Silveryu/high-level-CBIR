53438d27e89c480fc27183176ba1d81494b78f82
Pedro Santos <pedroamaralsantos@ua.pt>
2018-10-04 17:28:35 +0100

    added some code from tutorials

---
+++ tutorial_assigments/assigment_1/code/changingContrastBright/CMakeFiles/changingContrastBright.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rute/Documents/cadeiras/5ano/vc/tutorial/changingContrastBright/bright.cpp" "/home/rute/Documents/cadeiras/5ano/vc/tutorial/changingContrastBright/CMakeFiles/changingContrastBright.dir/bright.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
