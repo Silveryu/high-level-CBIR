78f935b782020bca8e750ad944ae09d29d16927a
Pedro Santos <pedroamaralsantos@ua.pt>
2018-10-18 20:00:46 +0100

    added code of the 2 first exercises

---
+++ tutorial_assigments/assigment_4/cameraCalibration/CMakeFiles/calib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calib.dir/chessboard.cpp.o"
+  "calib.pdb"
+  "calib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
