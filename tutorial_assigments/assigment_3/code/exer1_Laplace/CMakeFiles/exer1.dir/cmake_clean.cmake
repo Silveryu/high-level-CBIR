bafa7c5afb30d15c3322c36f2b9905ce0d28608a
Pedro Santos <pedroamaralsantos@ua.pt>
2018-10-18 17:36:47 +0100

    added more code of assigment 3; added report

---
+++ tutorial_assigments/assigment_3/code/exer1_Laplace/CMakeFiles/exer1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exer1.dir/sober.cpp.o"
+  "exer1.pdb"
+  "exer1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exer1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
